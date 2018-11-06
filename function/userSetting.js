7b8e6fad094d4230a8b91ef12e20113afcb599fe
Grekur <gregor.kury@hotmail.com>
2018-11-06 19:36:08 +0100

    Created FlightSettingsClass to have all values in one property in local storage

--- function/userSetting.js
+++ function/userSetting.js
@@ -1,5 +1,5 @@
 // Debug variable 
-var debug = 1;
+var debug = 0;
 /*-------------------------------------------------------------------------------------------------------------------------------*/
 // SECTION: Form functionalities 
 // Get modal element
@@ -124,6 +124,51 @@ for (let i = 0; i < userJourneySettingsLocalStorage.length; i++) {
 if(debug == 1){
 console.log(lowestRtDate.userRtDate);}
 
+/*-------------------------------------------------------------------------------------------------------------------------------*/
+// TO DO: MAKE A FUNCTION OUT OF IT, OTHERWISE IT WILL ALWAYS ADD A NEW ITEM TO LOCAL STORAGE WHEN YOU REFRESH 
+// SECTION: Create Flight Setting Class
+// Combine all values needed to select flights from local storage
+class flightSettings {
+
+  // The constructor for our class, which will allow us to create new objects of our class
+  constructor(journeyId, journeyName, highestDpDate, lowestRtDate, lowestUserBudget, journeyDp,journeyRegion) {
+      this.journeyId = journeyId;
+      this.journeyName = journeyName;
+      this.highestDpDate = highestDpDate;
+      this.lowestRtDate = lowestRtDate;
+      this.lowestUserBudget = lowestUserBudget;
+      this.journeyDp = journeyDp;
+      this.journeyRegion = journeyRegion;
+      }
+  };
+  // Create var called userFlightSettings
+  var journeyFlightSettings = JSON.parse(localStorage.getItem("journeyFlightSettings"));
+  
+  // Hardcoded users in journeyFlightSettings in the array
+  if(journeyFlightSettings === null){
+     journeyFlightSettings = [];
+     journeyFlightSettings.push(new flightSettings(1234, "John´s Journey", "2018-10-30", "2018-11-10", 400, "Copenhagen", "Europe"));
+  }
+  
+  // Defining varibales - as we get them from local storage
+  // You need to define a variable first to unpack the JSON String and then you can target single values 
+  var flightJourneySettingsLocalStorage = JSON.parse(localStorage.getItem("createdJourney"));
+  // How should the system know for which journey we are checking??? [1] -> müsste es selber herausfinden
+     journeyId = flightJourneySettingsLocalStorage[1].journeyId;
+     journeyName = flightJourneySettingsLocalStorage[1].journeyName;
+     highestDpDate = localStorage.getItem("highestDpDate");
+     lowestRtDate = localStorage.getItem("lowestRtDate");
+     lowestUserBudget = localStorage.getItem("lowestUserBudget");
+     journeyDp = journeyName = flightJourneySettingsLocalStorage[1].journeyDp;
+     journeyRegion = journeyName = flightJourneySettingsLocalStorage[1].journeyRegion;
+
+  // Push the values taken from the form into the journeyFlightSettings array 
+  journeyFlightSettings.push(new flightSettings (journeyId, journeyName, highestDpDate, lowestRtDate, lowestUserBudget, journeyDp,journeyRegion));
+   //  if(debug == 1){
+         console.log(journeyFlightSettings);
+
+  // Push journeyFlightSettings into local storage
+localStorage.setItem('journeyFlightSettings',JSON.stringify(journeyFlightSettings));
 /*-------------------------------------------------------------------------------------------------------------------------------*/
 /*
 // SECTION: Assign a nummerical number to every date of the year
